0601f88d50cb17d793463762f832be69d3669a03
Rutvi Shah <shahrutvi020@gmail.com>
2025-04-30 16:51:30 +0530

    Add files via upload

---
+++ STL/8.2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <sstream>
+
+int main() {
+    std::string sentence;
+    std::map<std::string, int> wordFrequency;
+
+    std::cout << "Enter a sentence: ";
+    std::getline(std::cin, sentence);
+
+    std::stringstream ss(sentence);
+    std::string word;
+
+    while (ss >> word) {
+        wordFrequency[word]++;
+    }
+
+    std::cout << "Word Frequency Distribution:\n";
+    for (const auto& pair : wordFrequency) {
+        std::cout << pair.first << ": " << pair.second << std::endl;
+    }
+
+    return 0;
+}
