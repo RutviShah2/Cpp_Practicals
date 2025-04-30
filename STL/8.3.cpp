533164e66ea4e8b47e6e0cde4a5bf07046bb4b6c
Rutvi Shah <shahrutvi020@gmail.com>
2025-04-30 16:51:54 +0530

    Add files via upload

---
+++ STL/8.3.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <set>
+
+int main() {
+    std::set<int> transactionIDs;
+    int n, transactionID;
+
+    std::cout << "Enter the number of transactions: ";
+    std::cin >> n;
+
+    std::cout << "Enter the transaction IDs: ";
+    for (int i = 0; i < n; ++i) {
+        std::cin >> transactionID;
+        transactionIDs.insert(transactionID);
+    }
+
+    std::cout << "Unique Transaction IDs in sorted order:\n";
+    for (auto it = transactionIDs.begin(); it != transactionIDs.end(); ++it) {
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
