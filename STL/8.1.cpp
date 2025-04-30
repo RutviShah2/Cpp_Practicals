4e1a3193f7e1109323a2515042cb7e84e3041ef6
Rutvi Shah <shahrutvi020@gmail.com>
2025-04-30 16:50:58 +0530

    Add files via upload

--- STL/8.1.cpp
+++ STL/8.1.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+#include <vector>
 
+int main() {
+    std::vector<int> sequence;
+    int n, element;
+
+    std::cout << "Enter the number of elements: ";
+    std::cin >> n;
+
+    std::cout << "Enter the elements: ";
+    for (int i = 0; i < n; ++i) {
+        std::cin >> element;
+        sequence.push_back(element);
+    }
+
+    std::cout << "Reversed sequence using iterators: ";
+    for (auto it = sequence.end() - 1; it >= sequence.begin(); --it) {
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
