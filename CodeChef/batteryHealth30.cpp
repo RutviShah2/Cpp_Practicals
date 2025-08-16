c8e863427fc368253715847466ea3680eabc92c1
Rutvi Shah <shahrutvi020@gmail.com>
2025-08-16 08:47:36 +0530

    added

---
+++ CodeChef/batteryHealth30.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        int X;
+        cin>>X;
+        if(X>=80)
+        {
+            cout<<"yes"<<endl;
+        }
+        else
+        {
+            cout<<"no"<<endl;
+        }
+    }
+}
