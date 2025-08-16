726c55cedc45e342de78d155bc27db48c3c58495
Rutvi Shah <shahrutvi020@gmail.com>
2025-08-16 09:04:27 +0530

    Updated

--- Pointers and DMA/6.2.cpp
+++ Pointers and DMA/6.2.cpp
@@ -1,18 +1,14 @@
 #include <iostream>
 using namespace std;
-
 class Point {
     int x, y;
-
 public:
     Point(int x = 0, int y = 0) : x(x), y(y) {}
-
     Point* shift(int dx, int dy) {
         x += dx;
         y += dy;
         return this;
     }
-
     void display() {
         cout << "(" << x << ", " << y << ")" << endl;
     }
@@ -20,11 +16,10 @@ public:
 
 int main() {
     Point p(5, 10);
-
     p.shift(2, 3)->shift(-1, 0)->shift(4, -2);
-
     cout << "Final Position: ";
     p.display();
 
     return 0;
 }
+
