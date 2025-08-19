580fcdb2ff755e2872ab07cafc36528f451243f8
Rutvi Shah <shahrutvi020@gmail.com>
2025-08-19 09:41:02 +0530

    initial commit

---
+++ CodeChef/lunchtime59.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int T;
+    cin >> T; 
+    while (T--) {
+       int X;
+       cin>>X;
+       if(X>=1 && X<=4)
+       {
+           cout<<"YES"<<endl;
+       }
+       else
+       {
+           cout<<"NO"<<endl;
+       }
+    }
+    return 0;
+}
