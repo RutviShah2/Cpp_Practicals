19c20484302187c528730c64b461fbb8a5a715a5
Rutvi Shah <shahrutvi020@gmail.com>
2025-08-20 23:38:15 +0530

    initial commit

---
+++ CodeChef/theGift71.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int X,N,M;
+    cin>>X>>N>>M;
+    if((X+M)<=N)
+    {
+        cout<<"YES"<<endl;
+    }
+    else
+    {
+        cout<<"NO"<<endl;
+    }
+    return 0;
+}
