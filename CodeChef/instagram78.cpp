9f17e5fadaa38830938bf34ee7225a1f0970683a
Rutvi Shah <shahrutvi020@gmail.com>
2025-08-21 17:37:41 +0530

    initial commit

---
+++ CodeChef/instagram78.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int X,Y;
+        cin>>X>>Y;
+        if(X>(Y*10))
+        {
+            cout<<"YES"<<endl;
+        }
+        else
+        {
+            cout<<"NO"<<endl;
+        }
+}
+    return 0;
+}
