39cbed65726cda48e5770dcec1f398ee61c9eca2
Rutvi Shah <shahrutvi020@gmail.com>
2025-08-25 12:34:25 +0530

    initial commit

---
+++ CodeChef/creditScore95.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int X;
+    cin>>X;
+    if(X>=750)
+    {
+        cout<<"YES"<<endl;
+    }
+    else
+    {
+        cout<<"NO"<<endl;
+    }
+    return 0;
+}
