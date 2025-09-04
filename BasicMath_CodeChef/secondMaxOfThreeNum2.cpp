46eacbc87c6120df8b3a75f56fa4c2e1bcf3fb1f
Rutvi Shah <shahrutvi020@gmail.com>
2025-09-04 20:21:09 +0530

    Starred Problem

---
+++ BasicMath_CodeChef/secondMaxOfThreeNum2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main() {
+    int N;
+    cin >> N; 
+    while (N--) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        int arr[3] = {a, b, c};
+        sort(arr, arr + 3);
+        cout << arr[1] << endl; 
+    }
+    
+    return 0;
+}
