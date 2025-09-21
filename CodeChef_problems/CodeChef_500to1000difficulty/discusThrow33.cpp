2d16d02f108cdc83b355a576342c438a4efebd41
Rutvi Shah <shahrutvi020@gmail.com>
2025-09-21 22:42:12 +0530

    initial commit

---
+++ CodeChef_problems/CodeChef_500to1000difficulty/discusThrow33.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main() 
+{
+    int T;
+    cin >> T;
+    while (T--) 
+    {
+        int A, B, C;
+        cin >> A >> B >> C;
+        cout << max({A, B, C}) << endl;
+    }
+}
