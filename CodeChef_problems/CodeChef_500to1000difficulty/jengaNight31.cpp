05530d12aefcbd4f76b3c3795fe033198172c78a
Rutvi Shah <shahrutvi020@gmail.com>
2025-09-21 22:25:00 +0530

    initial commit

---
+++ CodeChef_problems/CodeChef_500to1000difficulty/jengaNight31.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        int N,X;
+        cin>>N>>X;
+        if (X >= N && X % N == 0)
+        {
+            cout << "YES"<<endl;
+        }
+        else
+        {
+            cout << "NO"<<endl;
+        }
+    }
+}
