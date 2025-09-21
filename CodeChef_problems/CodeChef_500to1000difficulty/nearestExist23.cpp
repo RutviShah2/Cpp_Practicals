796c023383ce5a6173b71872bd3dc93cc192e11b
Rutvi Shah <shahrutvi020@gmail.com>
2025-09-21 14:41:59 +0530

    initial commit

---
+++ CodeChef_problems/CodeChef_500to1000difficulty/nearestExist23.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        int X;
+        cin>>X;
+        if(X<=50)
+        {
+            cout<<"LEFT"<<endl;
+        }
+        else
+        {
+            cout<<"RIGHT"<<endl;
+        }
+    }
+}
