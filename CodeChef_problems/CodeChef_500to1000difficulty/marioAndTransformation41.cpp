c7015c15302fe0cfcfdbfe7c52b6e29d83fdf107
Rutvi Shah <shahrutvi020@gmail.com>
2025-09-22 23:03:23 +0530

    initial commit

---
+++ CodeChef_problems/CodeChef_500to1000difficulty/marioAndTransformation41.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+    int T;
+    cin >> T; 
+    while (T--) 
+    {
+        int X;
+        cin >> X;
+        int state = X % 3; 
+        if (state == 0)
+        {
+            cout << "NORMAL" << endl;
+        } 
+        else if (state == 1)
+        {
+            cout << "HUGE" << endl;
+        }
+        else 
+        {
+            cout << "SMALL" << endl;
+        }
+    }
+}
