de9d581611b23a148aeaf5c59f702b5e01b7f60e
Rutvi Shah <shahrutvi020@gmail.com>
2025-09-22 13:40:24 +0530

    initial commit

---
+++ CodeChef_problems/CodeChef_500to1000difficulty/policeAndThief36.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        int X,Y;
+        cin>>X>>Y;
+        if(X>Y)
+        {
+            cout<<X-Y<<endl;
+        }
+        else if(X<Y)
+        {
+            cout<<Y-X<<endl;
+        }
+        else
+        {
+            cout<<"0"<<endl;
+        }
+    }
+}
