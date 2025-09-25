eac169a5dd7a2b62fbb329997f8dce0e7c4704e9
Rutvi Shah <shahrutvi020@gmail.com>
2025-09-25 10:35:21 +0530

    initial commit

---
+++ CodeChef_problems/CodeChef_500to1000difficulty/findingSquareRoots47.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        int N;
+        cin>>N;
+        cout<<(int)pow(N,0.5)<<endl;
+    }
+}
