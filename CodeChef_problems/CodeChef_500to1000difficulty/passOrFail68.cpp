e8e52a66f782c4d546c16f29278cddf140d94d2c
Rutvi Shah <shahrutvi020@gmail.com>
2025-09-29 19:18:32 +0530

    initial commit

---
+++ CodeChef_problems/CodeChef_500to1000difficulty/passOrFail68.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int N,X,P;
+        cin>>N>>X>>P;
+        int score = (X * 3) - (N - X);
+        if(score >= P)
+        {
+            cout<<"PASS"<<endl;
+        } 
+        else
+        {
+            cout<<"FAIL"<<endl;
+        }
+    }
+}
