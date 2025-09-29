e22222c8a1acd6342a68723a15397a042d2e22b5
Rutvi Shah <shahrutvi020@gmail.com>
2025-09-29 20:20:40 +0530

    initial commit

---
+++ CodeChef_problems/CodeChef_500to1000difficulty/tooManyItems70.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int T;
+    cin>>T;
+    while (T--)
+    {
+        int N;
+        cin >> N;
+        cout << (N + 9) / 10 << endl;
+    }
+    return 0;
+}
