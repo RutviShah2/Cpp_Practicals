dc2a7abb4e6738c69f52b585f30e95bf95860d5b
Rutvi Shah <shahrutvi020@gmail.com>
2025-10-13 11:51:40 +0530

    first problem added

--- CodeChef_problems/Strings/dnaStorage.cpp
+++ CodeChef_problems/Strings/dnaStorage.cpp
@@ -1,1 +1,35 @@
-
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--) 
+    {
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        string encoded = "";
+        for (int i = 0; i < n; i += 2)
+        {
+            if (s[i] == '0' && s[i + 1] == '0')
+            {
+                encoded += 'A';
+            } 
+            else if (s[i] == '0' && s[i + 1] == '1')
+            {
+                encoded += 'T';
+            } 
+            else if (s[i] == '1' && s[i + 1] == '0') 
+            {
+                encoded += 'C';
+            } 
+            else {
+                encoded += 'G';
+            }
+        }
+        cout << encoded << endl;
+    }
+}
