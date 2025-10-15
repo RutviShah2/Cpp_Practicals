80b52c2a0af84d0daef247fa513b5d19f2e5d09d
Rutvi Shah <shahrutvi020@gmail.com>
2025-10-15 09:18:38 +0530

    initial commit

---
+++ CodeChef_problems/BasicMath/chefAndChocolates.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        int X,Y,Z;
+        cin>>X>>Y>>Z;
+        cout<<((X*5 + Y*10)/Z)<<endl;
+    }
+}
