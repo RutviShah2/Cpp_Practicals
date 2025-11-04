f37738afb8c028550cb03adfcf6488b2960e6261
Rutvi Shah <shahrutvi020@gmail.com>
2025-11-04 16:54:01 +0530

    initial commit

---
+++ CodeChef_problems/PracticeArrays/checkIfArrayIsSorted.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+    bool check(vector<int>& nums)
+     {
+        int count = 0;
+        int n = nums.size();
+        for (int i = 0; i < n; i++)
+        {
+            if (nums[i] > nums[(i + 1) % n]) 
+            {
+                count++;
+            }
+            if (count > 1) return false;
+        }
+        return true;
+    }
+};
+int main()
+{
+    Solution obj;
+    int n;
+    cin >> n;               
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++) 
+        cin >> nums[i];
+    if (obj.check(nums))
+        cout << "true";
+    else
+        cout << "false";
+}
